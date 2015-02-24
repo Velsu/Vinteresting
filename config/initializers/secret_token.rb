cf9bb5562cfdca97015de0c275f724f460052063
Velsu <riftmage50@gmail.com>
2015-02-24 16:31:23 +0100

    Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmrPinterest::Application.config.secret_key_base = 'b4a332ed08f0f27d8b5ae2b91cde422e34402d7b759e05fd52398872c6ffbc7b26dc65b373c7fb725b103a2623c28da8e944a5dd3f7098d4e61824810f3adadf'
